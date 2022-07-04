d6e7dbd7a62e59213d3fde302b4f555da3fdcc4f
Muhammad Awais Khan <66188894+EngrAwaisKhan@users.noreply.github.com>
2022-07-05 01:22:40 +0500

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ export const Header = () => {
     return(
         <div>
             <h1 className="text-center">Expense Tracker App</h1>
-            <h3>By M. Awais Khan</h3>
+            <h3 className="text-center">By M. Awais Khan</h3>
         </div>
     )
 }
